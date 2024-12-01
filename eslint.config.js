68d342f0ea13820b18ca5eaee6bc446cf41d6ae2
minaadel88 <40057585+minaadel88@users.noreply.github.com>
2024-12-01 20:40:59 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
@@ -27,33 +26,3 @@ export default tseslint.config(
     },
   }
 );
-=======
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
-
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  }
-);
->>>>>>> 63fa3fbe2539cce316c2a72eed1cdd0996492de0
