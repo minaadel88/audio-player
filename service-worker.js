63f50caa18f0358e3e1ba0d3fbb9cdb487c46fd1
minaadel88 <40057585+minaadel88@users.noreply.github.com>
2024-12-01 20:44:41 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-self.addEventListener('install', (event) => {
+self.addEventListener('install', () => {
   console.log('Service Worker: Installed');
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', () => {
   console.log('Service Worker: Activated');
 });
 
