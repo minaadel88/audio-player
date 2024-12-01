7643786f363a4f787cc7a66ea7bc5312f27123f1
minaadel88 <40057585+minaadel88@users.noreply.github.com>
2024-12-01 20:41:25 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
-=======
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
->>>>>>> 63fa3fbe2539cce316c2a72eed1cdd0996492de0
+
