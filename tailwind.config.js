8d64b3a7919e4af30a298e64d5f43823939b4e0f
minaadel88 <40057585+minaadel88@users.noreply.github.com>
2024-12-01 20:42:08 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -15,22 +14,4 @@ export default {
     },
   },
   plugins: [],
-=======
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
-  theme: {
-    extend: {
-      screens: {
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-        '2xl': '1536px',
-      },
-    },
-  },
-  plugins: [],
->>>>>>> 63fa3fbe2539cce316c2a72eed1cdd0996492de0
 };
